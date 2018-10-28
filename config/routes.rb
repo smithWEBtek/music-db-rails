288a9c5eb47945e053e3a0770c2489482d02bf12
smithwebtek <brad@smithwebtek.com>
2018-10-27 23:13:34 -0400

    Refactor routes.rb without api namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,14 @@
 Rails.application.routes.draw do
- 
+	
 	root 'songs#index'
 	get '/songs/:id/resources', to: 'songs#resources'
 	get '/songs/:id/collections', to: 'songs#collections'
 
 	get '/books', to: 'resources#books'
 
-	namespace :api do 
-	  resources :categories
-		resources :songs
-		resources :song_collections
-		resources :collections
-		resources :resources
-	end
+	resources :categories
+	resources :songs
+	resources :song_collections
+	resources :collections
+	resources :resources
 end
